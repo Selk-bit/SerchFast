b71b9f4e2c135d0995e5c0f37252dc4295fd2da9
unknown <selkellouti233@gmail.com>
2025-02-23 12:23:55 +0000

    correct reqs

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def generate_license():
     try:
         # Insert the generated license into the License table
         cursor.execute('''
-            INSERT INTO "License" (licenseKey, generatedAt, expirationDate, used, user_hash)
+            INSERT INTO license (licenseKey, generatedAt, expirationDate, used, user_hash)
             VALUES (%s, %s, %s, FALSE, NULL)
         ''', (license_key, generated_at, expiration_date))
 
